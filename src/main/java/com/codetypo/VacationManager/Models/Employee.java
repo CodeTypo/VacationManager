6c4abdc9978caf784502beb71a77e21754daa274
Jakub Skurjat <jakubskurjat99@gmail.com>
2021-05-01 22:15:12 +0200

    Created Employee class.

---
+++ src/main/java/com/codetypo/VacationManager/Models/Employee.java
@@ -0,0 +1,61 @@
+package com.codetypo.VacationManager.Models;
+
+public class Employee {
+
+    private int id;
+
+    private String login;
+
+    private String password;
+
+    private boolean isAdmin;
+
+    public Employee(String login, String password, boolean isAdmin) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
+}
