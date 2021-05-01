0868857176f79b0cf57c215427a7c5a1b257b5e3
Dawid Kupczyk <250065@student.pwr.edu.pl>
2021-05-01 21:25:30 +0200

    Initial commit

---
+++ src/main/java/com/codetypo/VacationManager/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.codetypo.VacationManager;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
